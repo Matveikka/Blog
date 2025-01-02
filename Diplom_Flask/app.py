fd9c3d0a107975bdb7a809873dc3686885f9d4b4
Matveikka <matwei.sklyarenko@yandex.ru>
2025-01-02 14:50:07 +0300

    .

--- Diplom_Flask/app.py
+++ Diplom_Flask/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 import re
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -18,6 +19,16 @@ def before_first_request():
 def generate_slug(title):
     slug = re.sub(r'[^a-zA-Z0-9-]', '-', title.lower())
     slug = re.sub(r'-+', '-', slug).strip('-')
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    original_slug = slug
+    count = cursor.execute('SELECT COUNT(*) FROM posts WHERE slug = ?', (slug,)).fetchone()[0]
+    i = 1
+    while count > 0:
+        slug = f"{original_slug}-{i}"
+        count = cursor.execute('SELECT COUNT(*) FROM posts WHERE slug = ?', (slug,)).fetchone()[0]
+        i += 1
+    close_db_connection(conn)
     return slug
 
 
@@ -40,22 +51,22 @@ def init_db():
         'rezume TEXT NOT NULL, '
         'info TEXT NOT NULL, '
         'created_at DATETIME DEFAULT CURRENT_TIMESTAMP, '
-        'slug UNIQUE TEXT NOT NULL)')
+        'slug TEXT UNIQUE NOT NULL)')
     conn.close()
 
 
 @app.route('/')
-def index():
+def all_posts():
     conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
+    posts = conn.execute('SELECT * FROM posts ORDER BY created_at DESC').fetchall()
     conn.close()
     return render_template('home.html', posts=posts)
 
 
-@app.route('/<string:post_slug>', strict_slashes=False)
-def get_post(post_slug):
+@app.route('/posts/<slug>', strict_slashes=False)
+def get_post(slug):
     conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE slug = ?', (post_slug,)).fetchone()
+    post = conn.execute('SELECT * FROM posts WHERE slug = ?', (slug,)).fetchone()
     conn.close()
     return render_template('details.html', post=post)
 
@@ -66,27 +77,25 @@ def new_post():
         title = request.form['title']
         rezume = request.form['rezume']
         info = request.form['info']
-        created_at = request.form['created_at']
+        created_at = datetime.now().isoformat()
         slug = generate_slug(title)
         conn = get_db_connection()
         conn.execute('INSERT INTO posts (title, rezume, info, created_at, slug) VALUES (?, ?, ?, ?, ?)',
                      (title, rezume, info, created_at, slug))
         conn.commit()
         conn.close()
-
-        return redirect(url_for('index'))
-
+        return redirect(url_for('all_posts'))
     return render_template('add_post.html')
 
 
-@app.route('/delete/<string:post_slug>', methods=['POST'])
-def delete_post(post_slug):
+@app.route('/posts/slug/delete', methods=['POST'])
+def delete_post(slug):
     conn = get_db_connection()
-    post = conn.execute('DELETE FROM posts WHERE slug = ?', (post_slug,))
+    post = conn.execute('DELETE FROM posts WHERE slug = ?', (slug,))
     conn.commit()
     conn.close()
     return render_template('after_delete.html', post=post)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
